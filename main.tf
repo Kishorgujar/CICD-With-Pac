94d908a0e4e01179e8efa508daced33bc718594b
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-08 16:02:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "random_pet" "name" {}
 # Create an EC2 instance
 resource "aws_instance" "web" {
   ami           = "ami-078264b8ba71bc45e"  # Check if this AMI is valid in ap-south-1
-  instance_type = "t3.medium"               # Free tier instance type
+  instance_type = "t2.micro"               # Free tier instance type
   subnet_id     = "subnet-0564d815852d0eb07"
   vpc_security_group_ids = ["sg-0e598455eeca7dd6c"]
   key_name      = "test-kyp"               # Use just the name of the key pair, without .pem
